6f4eab6b03f0007a272af4e936b046b25ea54083
Jose Anicama <j.j.anicamatanta@gmail.com>
2026-01-06 17:52:00 -0500

    feat: configure Livewire for subdirectory deployment in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Configure Livewire for subdirectory deployment
+        if (config('app.env') === 'production') {
+            Livewire::setUpdateRoute(function ($handle) {
+                return Route::post('/ms_crm/livewire/update', $handle);
+            });
+            
+            Livewire::setScriptRoute(function ($handle) {
+                return Route::get('/ms_crm/livewire/livewire.js', $handle);
+            });
+        }
     }
 }
